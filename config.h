06be8b48a03c1bb57fb0e665bbd0f9a2594490ca
John Higgins <johnmatthiggins@gmail.com>
2025-04-15 22:14:21 -0700

    switched to monokai charcoal for theme
    shoutout to termcolors.dev for making this easy

--- config.h
+++ config.h
@@ -93,37 +93,34 @@ char *termname = "onigiri";
  */
 unsigned int tabspaces = 8;
 
-/* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"#4e4e4e",
-	"#ff6b60",
-	"#a7ff60",
-	"#ffffb6",
-	"#96cafd",
-	"#ff73fd",
-	"#c6c4fd",
-	"#eeeeee",
+    /* 8 normal colors */
+    "#1a1a1a",
+    "#f4005f",
+    "#98e024",
+    "#fa8419",
+    "#9d65ff",
+    "#f4005f",
+    "#58d1eb",
+    "#c4c5b5",
 
-	/* 8 bright colors */
-	"#7c7c7c",
-	"#ffb6b0",
-	"#ceffab",
-	"#ffffcb",
-	"#b5dcfe",
-	"#ff9cfe",
-	"#dfdffe",
-	"#ffffff",
+    /* 8 bright colors */
+    "#625e4c",
+    "#f4005f",
+    "#98e024",
+    "#e0d561",
+    "#9d65ff",
+    "#f4005f",
+    "#58d1eb",
+    "#f6f6ef",
 
-	[255] = 0,
+    [255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#0000a3",
-	"#ffff4d",
-	"#ffa460", /* default foreground colour */
-	"#0000a3", /* default background colour */
+    "#000000",
+    "#FFFFFF",
+    "#FFFFFF",
+    "#000000",
 };
-
 
 /*
  * Default colors (colorname index)
@@ -133,6 +130,47 @@ unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 259;
+
+// /* Terminal colors (16 first used in escape sequence) */
+// static const char *colorname[] = {
+// 	/* 8 normal colors */
+// 	"#4e4e4e",
+// 	"#ff6b60",
+// 	"#a7ff60",
+// 	"#ffffb6",
+// 	"#96cafd",
+// 	"#ff73fd",
+// 	"#c6c4fd",
+// 	"#eeeeee",
+//
+// 	/* 8 bright colors */
+// 	"#7c7c7c",
+// 	"#ffb6b0",
+// 	"#ceffab",
+// 	"#ffffcb",
+// 	"#b5dcfe",
+// 	"#ff9cfe",
+// 	"#dfdffe",
+// 	"#ffffff",
+//
+// 	[255] = 0,
+//
+// 	/* more colors can be added after 255 to use with DefaultXX */
+// 	"#0000a3",
+// 	"#ffff4d",
+// 	"#ffa460", /* default foreground colour */
+// 	"#0000a3", /* default background colour */
+// };
+//
+//
+// /*
+//  * Default colors (colorname index)
+//  * foreground, background, cursor, reverse cursor
+//  */
+// unsigned int defaultfg = 257;
+// unsigned int defaultbg = 256;
+// unsigned int defaultcs = 258;
+// static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
