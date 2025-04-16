fe5b0d8ca015b3df7d153676853f0e9f5f1d44e3
John Higgins <johnmatthiggins@gmail.com>
2025-04-16 06:09:37 -0700

    removed commented out theme variables

--- config.h
+++ config.h
@@ -74,7 +74,7 @@ static unsigned int cursorthickness = 2;
 static int bellvolume = 0;
 
 /* default TERM value */
-char *termname = "onigiri";
+char *termname = "st";
 
 /*
  * spaces per tab
@@ -130,47 +130,6 @@ unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 259;
-
-// /* Terminal colors (16 first used in escape sequence) */
-// static const char *colorname[] = {
-// 	/* 8 normal colors */
-// 	"#4e4e4e",
-// 	"#ff6b60",
-// 	"#a7ff60",
-// 	"#ffffb6",
-// 	"#96cafd",
-// 	"#ff73fd",
-// 	"#c6c4fd",
-// 	"#eeeeee",
-//
-// 	/* 8 bright colors */
-// 	"#7c7c7c",
-// 	"#ffb6b0",
-// 	"#ceffab",
-// 	"#ffffcb",
-// 	"#b5dcfe",
-// 	"#ff9cfe",
-// 	"#dfdffe",
-// 	"#ffffff",
-//
-// 	[255] = 0,
-//
-// 	/* more colors can be added after 255 to use with DefaultXX */
-// 	"#0000a3",
-// 	"#ffff4d",
-// 	"#ffa460", /* default foreground colour */
-// 	"#0000a3", /* default background colour */
-// };
-//
-//
-// /*
-//  * Default colors (colorname index)
-//  * foreground, background, cursor, reverse cursor
-//  */
-// unsigned int defaultfg = 257;
-// unsigned int defaultbg = 256;
-// unsigned int defaultcs = 258;
-// static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
